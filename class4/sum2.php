d177af8d82b1b08ab08250567c2579b40942163e
coutinho21 <guilhermecscoutinho@gmail.com>
2023-03-15 10:09:20 +0000

    Finished class4 exercise 1

---
+++ class4/sum2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sum 2 numbers</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        $num1 = $_GET["num1"];
+        $num2 = $_GET["num2"];
+        $sum = $num1 + $num2;
+        echo "$num1 + $num2 = $sum\n";
+        echo "<br/> <br/>";
+
+        echo "<a href= \"form2.html\">Do another sum.</a>";
+    ?>
+</body>
+</html>
