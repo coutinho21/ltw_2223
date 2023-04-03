2f12267e3b75d3eaf68d30ea824e84847f3d0f35
coutinho21 <guilhermecscoutinho@gmail.com>
2023-04-04 00:25:39 +0100

    fix on ex8

--- class4/news/action_edit_news.php
+++ class4/news/action_edit_news.php
@@ -1,9 +1,9 @@
 <?php
+    session_start();
     if(!isset($_SESSION['username']))
         header('Location: index.php');
     require_once('database/connection.php');
     require_once('database/news.php');
-    session_start();
     $db = getDatabaseConnection();
     $id = $_POST['id'];
     $title = $_POST['title'];
